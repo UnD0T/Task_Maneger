19e2cfb53a4218fa48dacf0f1f61b89d7d90a186
UnD0T <ammiha230208@gmail.com>
2024-03-06 19:32:50 +0200

    my first bot

--- APP.py
+++ APP.py
@@ -1,1 +1,22 @@
-git add -A
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from decouple import config
+
+bot = Bot(token=config("TOKEN"))
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("This is answer for start command")
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("echo echo")
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
